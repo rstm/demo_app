ec7f66789125f6d3196b47ddae057e5dc5d57568
rstm <m.rustem92@gmail.com>
2014-12-16 18:11:04 +0330

    root страница открывается

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ DemoApp::Application.routes.draw do
 
   resources :users
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
